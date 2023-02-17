dbbab9330e54990594f02076a4097f415f7cc1ce
MUADH BIN MOHAMMED ALI <100176376+MuadhAli@users.noreply.github.com>
2023-02-17 12:12:19 +0530

    updated the client

--- client.py
+++ client.py
@@ -12,26 +12,40 @@ clientSocket.connect((hostIp, portNumber))
 
 window = Tk()
 window.title("Connected To: "+ hostIp+ ":"+str(portNumber))
+window.geometry("500x500")
 
-txtMessages = Text(window, width=50)
-txtMessages.grid(row=0, column=0, padx=10, pady=10)
+messagesFrame = Frame(window)
+messagesFrame.pack(fill=BOTH, expand=True)
+messagesFrame.grid_columnconfigure(0, weight=1)
+messagesFrame.grid_rowconfigure(0, weight=1)
 
-txtYourMessage = Entry(window, width=50)
+txtMessages = Text(messagesFrame, width=50)
+txtMessages.pack(fill=BOTH, padx=10, pady=10, expand=True)
+
+scrollbar = Scrollbar(messagesFrame)
+scrollbar.pack(side=RIGHT, fill=Y)
+txtMessages.config(yscrollcommand=scrollbar.set)
+scrollbar.config(command=txtMessages.yview)
+
+messageFrame = Frame(window)
+messageFrame.pack(fill=X, side=BOTTOM)
+
+txtYourMessage = Entry(messageFrame, width=50)
 txtYourMessage.insert(0,"Your message")
-txtYourMessage.grid(row=1, column=0, padx=10, pady=10)
+txtYourMessage.pack(side=LEFT, padx=10, pady=10, expand=True)
 
 def sendMessage():
     clientMessage = txtYourMessage.get()
     txtMessages.insert(END, "\n" + "You: "+ clientMessage)
     clientSocket.send(clientMessage.encode("utf-8"))
+    txtYourMessage.delete(0, END)
 
-btnSendMessage = Button(window, text="Send", width=20, command=sendMessage)
-btnSendMessage.grid(row=2, column=0, padx=10, pady=10)
+btnSendMessage = Button(messageFrame, text="Send", width=20, command=sendMessage)
+btnSendMessage.pack(side=RIGHT, padx=10, pady=10)
 
 def recvMessage():
     while True:
         serverMessage = clientSocket.recv(1024).decode("utf-8")
-        print(serverMessage)
         txtMessages.insert(END, "\n"+serverMessage)
 
 recvThread = Thread(target=recvMessage)
@@ -39,8 +53,3 @@ recvThread.daemon = True
 recvThread.start()
 
 window.mainloop()
-
-
-
-
-
